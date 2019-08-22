c168b616f082993e371dda8ae0159e53a9fc4045
lavu <leeanhvux2111@gmail.com>
2019-08-22 11:28:14 +0700

    intergrate api

--- log_reader.py
+++ log_reader.py
@@ -14,22 +14,70 @@ def read_file(file_path_name):
 
 
 def parse_frag(log_info, pattern_dict):
-    time = ''
-    for type, pattern in pattern_dict.items():
-        re_result = search(pattern, log_info)
-        if re_result:
-            return eval('handel_' + pattern_name + '(' + re_result + ',')
-        else:
-            return None
+    """
+    :param log_info: (str) a line or an element in the log file
+    :param pattern_dict: (dict) dictionary with key is the type of log_info and
+    value is the pattern for regex
+    :return: (list) infomation get from log_info
+    """
+    for info_type, pattern in pattern_dict.items():
+        try:
+            regex_result = search(pattern, log_info)
+        except TypeError as Error:
+            stderr.write(Error)
+        if regex_result:
+            try:
+                return eval('handel_' + info_type + '(' + regex_result + ',')
+            except (NameError, TypeError) as Error:
+                stderr.write(Error)
+                
+
+def check_if_hour_increase(time_1, time_2):
+    if (int(time_1[0]) * 60 + int(time_1[1])) > (int(time_2[0]) * 60 + int(time_2[1])):
+        return True
+    return False
 
 
-def handle_frag(info_group):
+def handle_frag(regex_result):
+    info_list = regex_result.groups()
+    if len(info_list) == 5:
+        frag_info_dict = {
+            'suicide':False,
+            'time':{
+                'minute':info_list[0],
+                'second':info_list[1]},
+            'killer':info_list[2],
+            'weapon':info_list[3],
+            'victim':info_list[4]
+        }
+    elif len(info_list) == 3:
+        frag_info_dict = {
+            'suicide':True,
+            'time':{
+                'minute':info_list[0],
+                'second':info_list[1]},
+            'killer':None,
+            'weapon':None,
+            'victim':info_list[2]
+        }
+    return frag_info_dict
+
+
+def handle_start_time(regex_result):
     pass
 
 
-def handle_start_time(info_group):
+def handle_end_time(regex_result):
     pass
+
+
 
 if __name__ == '__main__':
-    pass
-
+    pattern_dict = {
+        'frag' : '(.*):(.*)> <Lua> ([^ ]*) killed ([^ ]*) \
+            with ([^ \n]*)|<(.*):(.*)> <Lua> ([^ ]*) killed itself',
+        'start_time':'',
+        'end_time':'',
+        'normal_info_line':'<([0-5][0-9]):([0-5][0-9])>',
+        'player_name':"OS User name: '(.*)'"
+    }
